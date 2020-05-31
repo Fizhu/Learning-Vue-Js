537a396a2081c3b50a75401ff56d930bd64c2001
fizhu <hvyz.anbiya@gmail.com>
2020-06-01 01:34:37 +0700

    add vuetify

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    </v-app-bar>
+
+    <v-content>
+      <Test/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Test from './components/Test';
+
+export default {
+  name: 'App',
+
+  components: {
+    Test,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/X.vue
@@ -0,0 +1,21 @@
+// export default {
+//   name: 'Test',
+//   props: {
+//     msg: String
+//   },
+//   mounted() {
+//     this.load()
+//   },
+//   methods: {
+//     load(){
+//         let url = 'https://bus.unila.ac.id/crud/api/v1/getlistbarang.php';
+//         axios.get(url).then(res => {
+//         this.data = res.data.data
+//         console.log(res.data)
+//       }).catch ((err) => {
+//         console.log(err);
+//       })
+//     }
+//    }
+// }
+</script>
